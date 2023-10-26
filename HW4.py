a61dee7edd10ce736d2240b35bcf8d06575720f1
Alexandr <118203209+BandrD@users.noreply.github.com>
2023-10-26 16:00:12 +0500

    Create HW4.py

---
+++ HW4.py
@@ -0,0 +1,27 @@
+import csv
+
+aver_salary = 0
+items = list()
+
+with open('W4', newline='\n', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter=',')
+    for row in reader:
+        # print(row)
+        item = {
+            'number': int(row[0]),
+            'name': row[2] + ' ' + row[1],
+            'age': int (row[3]),
+            'salary': int(row[4][0:-1])
+        }
+
+        aver_salary += item['salary']
+        items.append(item)
+
+# print(items)
+
+aver_salary /= len(items)
+
+filtered = list()
+for item in items:
+    if(item['salary'] > aver_salary) and item['age'] > 25 + 27 % 10:
+        filtered.append(item)
